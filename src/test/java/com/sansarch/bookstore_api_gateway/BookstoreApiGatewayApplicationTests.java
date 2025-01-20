728bad72b9cf5b6a9771ca2064325d6e9153f0de
Santiago Negreira <sansfleitas@gmail.com>
2025-01-20 15:30:14 -0300

    Initialize project

---
+++ src/test/java/com/sansarch/bookstore_api_gateway/BookstoreApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansarch.bookstore_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
